7c188884b05216885a95234a83f582549abf563d
NotRayaan <157511314+0kimo@users.noreply.github.com>
2024-09-24 10:19:01 +0000

    update

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,8 @@
 const { writeInfo } = require("../helpers/logger");
 
 /** @param {import('discord.js').Client} client */
-module.exports = (client) => {
+module.exports = async (client) => {
   writeInfo(`${client.user.username} is ready!`);
+
+  //await require("../../scripts/deploy").syncCommands(client.commands);
 };
