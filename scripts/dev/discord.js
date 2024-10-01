35e06a9f1226110353e5b5c02c29085a99112f5e
NotRayaan <157511314+0kimo@users.noreply.github.com>
2024-10-01 15:34:36 +0000

    Fixed regexp, json -> js

---
+++ scripts/dev/discord.js
@@ -0,0 +1,37 @@
+module.exports = {
+  third_party_invite_sources: [
+    {
+      location: "discordservers.com",
+      redirect: "https://discordservers.com/server",
+      regexp:
+        /(?:^|\b)discordservers\.com\/server\/(?<vanity>[\w-]{2,225})(?:$|\b)/gi,
+    },
+    {
+      location: "disc.gg",
+      redirect: "https://disc.gg/",
+      regexp: /(?:^|\b)disc\.gg\/(?<vanity>[\w-]{2,225})(?:$|\b)/gi,
+    },
+    {
+      location: "disboard.org",
+      redirect: "https://disboard.org/server/join/",
+      regexp:
+        /(?:^|\b)disboard\.org\/server\/join\/(?<id>[\d-]{18,22})(?:$|\b)/gi,
+    },
+    {
+      location: "discadia.com",
+      redirect: "https://discadia.com/",
+      regexp: /(?:^|\b)discadia\.com\/(?<vanity>[\w-]{2,225})(?:$|\b)/gi,
+    },
+    {
+      location: "discords.com",
+      redirect: "https://discords.com/servers/",
+      regexp:
+        /(?:^|\b)discords\.com\/servers\/(?<vanity>[\w-]{2,225})(?:$|\b)/gi,
+    },
+    {
+      location: "discordfy.com",
+      redirect: "https://discordfy.com/",
+      regexp: /(?:^|\b)discordfy\.com\/(?<vanity>[\w-]{2,225})(?:$|\b)/gi,
+    },
+  ],
+};
