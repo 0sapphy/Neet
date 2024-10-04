edcc8fb717afba7ff5798830cd3eb3bb2b2cec2a
NotRayaan <157511314+0kimo@users.noreply.github.com>
2024-10-04 21:24:25 +0000

    feat: [lib] 0.0.3 default to true

--- lib/Client/Neet.ts
+++ lib/Client/Neet.ts
@@ -1,7 +1,7 @@
 import { Client, Collection, ClientOptions } from "discord.js";
 import { INeetCommand } from "../Types/neet";
 
-export class Neet<T extends boolean = false> extends Client<T> {
+export class Neet<T extends boolean = true> extends Client<T> {
   public constructor(options: ClientOptions) {
     super(options);
   }
