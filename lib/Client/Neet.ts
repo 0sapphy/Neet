0eb1638c8b06e35759c6e53445cbd3a39dd0de74
NotRayaan <157511314+0kimo@users.noreply.github.com>
2024-10-04 21:21:56 +0000

    feat: [lib] 0.0.2 Generic Type

--- lib/Client/Neet.ts
+++ lib/Client/Neet.ts
@@ -1,7 +1,7 @@
 import { Client, Collection, ClientOptions } from "discord.js";
 import { INeetCommand } from "../Types/neet";
 
-export class Neet extends Client {
+export class Neet<T extends boolean = false> extends Client<T> {
   public constructor(options: ClientOptions) {
     super(options);
   }
