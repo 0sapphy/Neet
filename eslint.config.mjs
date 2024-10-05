e0c55c3287be5709af648e5ee825c1c165e2652a
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2024-10-05 09:31:38 +0000

    Typescript ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.js", "./src/*"], languageOptions: {sourceType: "commonjs"}},
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
