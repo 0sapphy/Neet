f1a483031d0f1233212bc4ec18bc31deb7380764
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2024-10-06 10:28:42 +0000

    ***

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,6 +1,11 @@
 import emojis from "../../scripts/dev/emojis.json";
+import discord from "../../scripts/dev/discord";
 import { formatEmoji } from "discord.js";
-//const { third_party_invite_sources } = require("../../scripts/dev/discord");
+
+interface InviteLink {
+  matched: boolean;
+  codes: string[];
+}
 
 export function emoji(
   name: keyof typeof emojis,
@@ -10,48 +15,8 @@ export function emoji(
   return raw ? emoji : formatEmoji(emoji.id, emoji.animated);
 }
 
-export function checkInviteLink(content: string) {
-  /*const discord_invite_regexp =
-    /(?:^|\b)discord(?:(?:app)?\.com\/invite|\.gg(?:\/invite)?)\/(?<code>[\w-]{2,255})(?:$|\b)/gi;
-
-  const match = content.match(discord_invite_regexp);
- 
-  // if no matches found using the above RegExp, check for third party links.
-  if (!match) {
-    for (const source of third_party_invite_sources) {
-      const thirdparty_match = content.match(source.regexp);
-
-      // if match, return.
-      if (thirdparty_match) {
-        return {
-          found: true,
-          matches: thirdparty_match,
-          codes: getInviteLinkCodes(thirdparty_match),
-        };
-      }
-    }
-
-    return {
-      found: false,
-    };
-  }
-
-  return {
-    found: true,
-    matches: match,
-    codes: getInviteLinkCodes(match),
-  };*/
+export function searchInviteLinks(content: string) {
+  const official = content.match(discord.official);
 }
 
-/**
- * @param {string} links
- */
-export function getInviteLinkCodes(links: string[]) {
-  const codes = [];
-
-  for (const link of links) {
-    codes.push(link.split("/").pop());
-  }
-
-  return codes;
-}
+export function getInviteLinkCodes(links: string[]) {}
