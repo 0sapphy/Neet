722607c42901d940c0e6646f486fe45a15236e72
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2024-10-08 20:54:02 +0000

    feat(/member whois): Basic command logic

--- src/commands/member/whois.ts
+++ src/commands/member/whois.ts
@@ -1,5 +1,38 @@
-import { ChatInputCommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
+import { emoji } from "../../helpers/utils";
 
-export function run(interaction: ChatInputCommandInteraction) {
-    interaction.reply({ content: 'Hi' });
+export async function run(interaction: ChatInputCommandInteraction<"cached">) {
+    await interaction.deferReply();
+
+    let ack = false;
+    const embed = new EmbedBuilder();
+    const user = interaction.options.getUser('user', true);
+
+    // * Check if the user is in the server.
+    const member = interaction.options.getMember('user')
+    if (member) {
+        embed
+        .setAuthor({ name: member.displayName, iconURL: member.displayAvatarURL() })
+        .setTitle(`${emoji('Checkmark')} Information | ${user.username}`)
+        .setDescription(`**»** Username: ${user.username}\n**»** Created At: ${user.createdAt}\n**»** Joined At: ${member.joinedAt}`)
+        .setColor(member.roles.highest.hexColor ? member.roles.highest.hexColor : "Blurple")
+        .setTimestamp();
+
+        ack = true
+    }
+
+    if (!ack) {
+        embed
+        .setAuthor({ name: user.username, iconURL: user.displayAvatarURL() })
+        .setTitle(`${emoji('Checkmark')} Information | ${user.username}`)
+        .setDescription(`**»** Username: ${user.username}\n**»** Created At: ${user.createdAt}`)
+        .setColor('Blurple')
+        .setTimestamp();
+
+        ack = true;
+    }
+
+    // TODO: Case buttons
+
+    return interaction.editReply({ embeds: [embed] });
 }
