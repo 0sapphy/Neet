87af9e06251deb4208b955d7fedc57cf94758265
Shook <160603390+Rayandevs@users.noreply.github.com>
2024-10-19 21:45:05 +0000

    feat: neet-canvas

--- src/events/GuildMemberAdd.ts
+++ src/events/GuildMemberAdd.ts
@@ -1,14 +1,14 @@
 import { AttachmentBuilder, EmbedBuilder, Events } from "discord.js";
 import { NeetEvent } from "../../lib";
-import { createCanvas, loadImage } from "@napi-rs/canvas";
-import Settings from "../models/Settings";
+import { CanvaBuilder, Templates } from "neet-canvas.js";
+import { Setting } from "../models/Settings";
 
 export default new NeetEvent<"guildMemberAdd">({
   name: Events.GuildMemberAdd,
   once: false,
   run: async (member) => {
     const { guild, user } = member;
-    const data = await Settings.findOne({ guildId: guild.id });
+    const data = await Setting.findOne({ guildId: guild.id });
     if (
       !data ||
       data.welcome?.enabled != true ||
@@ -16,56 +16,16 @@ export default new NeetEvent<"guildMemberAdd">({
     )
       return;
 
-    const canvas = createCanvas(1772, 633);
-    const ctx = canvas.getContext("2d");
-    const background = await loadImage("./assets/welcome.png");
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = "#295F98";
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    const image = new CanvaBuilder({ template: Templates.WelcomeDefault }).set({
+      username: user.username,
+      members: guild.memberCount,
+      avatarURL: user.displayAvatarURL(),
+    });
 
-    if (user.username.length > 13) {
-      ctx.font = "bold 100px Genta";
-      ctx.fillStyle = member.displayHexColor
-        ? member.displayHexColor
-        : "#08C2FF";
-      ctx.fillText(user.username, 720, canvas.height / 2 + 5);
-    } else {
-      ctx.font = "bold 110px Genta";
-      ctx.fillStyle = member.displayHexColor
-        ? member.displayHexColor
-        : "#08C2FF";
-      ctx.fillText(user.username, 720, canvas.height / 2 + 5);
-    }
+    const ctx = await image.draw(image.canvas);
 
-    ctx.font = "bold 70px Genta";
-    ctx.fillStyle = "#FF6868";
-    ctx.fillText("#", canvas.width - 255, canvas.height / 2 + 5);
-
-    ctx.font = "bold 70px Genta";
-    ctx.fillStyle = "#9EA1D4";
-    ctx.fillText(
-      `${guild.memberCount}`,
-      canvas.width - 200,
-      canvas.height / 2 + 5,
-    );
-
-    ctx.font = "bold 60px Genta";
-    ctx.fillStyle = "#AAD7D9";
-    ctx.fillText(guild.name, 700, canvas.height / 2 - 150);
-
-    ctx.beginPath();
-    ctx.fillStyle = "#295F98";
-    ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.clip();
-
-    const avatarImage = await loadImage(
-      user.displayAvatarURL({ extension: "png" }),
-    );
-    ctx.drawImage(avatarImage, 65, canvas.height / 2 - 250, 500, 500);
-
-    const attachment = new AttachmentBuilder(canvas.toBuffer("image/png"), {
-      name: "welcome.png",
+    const attachment = new AttachmentBuilder(ctx, {
+      name: "welcome.jpeg",
       description: "Neet: Generated Welcome Image.",
     });
 
@@ -73,7 +33,7 @@ export default new NeetEvent<"guildMemberAdd">({
       .setAuthor({ name: user.username })
       .setThumbnail(guild.iconURL())
       .setDescription(`**${user.username}** joined **${guild.name}**.`)
-      .setImage("attachment://welcome.png")
+      .setImage("attachment://welcome.jpeg")
       .setTimestamp()
       .setColor("Blurple");
 
---
+++ src/events/GuildMemberRemove.ts
@@ -0,0 +1,46 @@
+import { AttachmentBuilder, EmbedBuilder, Events } from "discord.js";
+import { NeetEvent } from "../../lib";
+import { Setting } from "../models/Settings";
+import { CanvaBuilder, Templates } from "neet-canvas.js";
+
+export default new NeetEvent<"guildMemberRemove">({
+  name: Events.GuildMemberRemove,
+  once: false,
+  run: async (member) => {
+    const { guild, user } = member;
+    const data = await Setting.findOne({ guildId: guild.id });
+    if (
+      !data ||
+      data.farewell?.enabled != true ||
+      typeof data.farewell.channelId != "string"
+    )
+      return;
+
+    const image = new CanvaBuilder({ template: Templates.FarewellDefault }).set(
+      {
+        username: user.username,
+        members: guild.memberCount,
+        avatarURL: user.displayAvatarURL(),
+      },
+    );
+
+    const buffer = await image.draw(image.canvas);
+
+    const attachment = new AttachmentBuilder(buffer, {
+      name: "farewell.jpeg",
+      description: "Neet: Farewell Image",
+    });
+
+    const embed = new EmbedBuilder()
+      .setAuthor({ name: user.username })
+      .setThumbnail(guild.iconURL())
+      .setImage("attachment://farewell.jpeg")
+      .setDescription(`**${user.username}** left **${guild.name}**`)
+      .setColor("Orange")
+      .setTimestamp();
+
+    const channel = guild.channels.cache.get(data.farewell.channelId);
+    if (!channel?.isSendable()) return;
+    channel.send({ embeds: [embed], files: [attachment] });
+  },
+});
