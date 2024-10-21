5ecad98e2e5388f871ff8c1b27820cccf3d1ddc6
Shook <160603390+Rayandevs@users.noreply.github.com>
2024-10-21 22:31:44 +0000

    Migrate to use the new Button Parser

--- src/events/SelectMenuCreate.ts
+++ src/events/SelectMenuCreate.ts
@@ -1,33 +1,26 @@
 import { Events } from "discord.js";
-import { Neet, NeetButton, NeetEvent } from "../../lib";
+import { Neet, NeetEvent, parseId } from "../../lib";
 import { writeError } from "../helpers/logger";
 
 export default new NeetEvent<"interactionCreate">({
   name: Events.InteractionCreate,
   once: false,
   run: async (interaction) => {
+    if (!interaction.isAnySelectMenu()) return;
     const client = interaction.client as Neet;
     client.emit("cl-debug", "Recieved selectMenu interaction.");
 
     if (interaction.isChannelSelectMenu()) {
-      const parsed = NeetButton.prototype.parseId(interaction.customId);
+      const parsed = parseId(interaction.customId);
 
       try {
         client.emit("cl-debug", `Handing channelSelectMenu interaction.`);
 
-        if (parsed.identifiers.subId) {
-          (
-            await import(
-              `../application/selectMenu/channel/${parsed.identifiers.id}/${parsed.identifiers.subId}`
-            )
-          ).run(interaction, parsed.parameters);
-        } else {
-          (
-            await import(
-              `../application/selectMenu/channel/${parsed.identifiers.id}`
-            )
-          ).run(interaction, parsed.parameters);
-        }
+        (
+          await import(
+            `../application/selectMenu/channel/${parsed.id}/${parsed.sub_id}`
+          )
+        ).run(interaction, parsed.args);
       } catch (error) {
         writeError("ChannelSelect", error);
       }
