56b0a808d50658fca507a2452799b4d8f9985198
Shook <160603390+Rayandevs@users.noreply.github.com>
2024-10-21 22:32:13 +0000

    chore: Add MessageOptions

--- src/models/Settings.ts
+++ src/models/Settings.ts
@@ -1,12 +1,14 @@
 import {
   getModelForClass,
+  modelOptions,
   Passthrough,
   prop,
   ReturnModelType,
+  Severity,
 } from "@typegoose/typegoose";
 import { QueryOptions } from "mongoose";
 
-function UTILMakeUpdatable(object: UpdateQueryAnyGreeting) {
+export function UTILMakeUpdatable(object: UpdateQueryAnyGreeting) {
   const makeProperty = (prop: string): string => `${object.prefix}.${prop}`;
   const data = {};
 
@@ -15,29 +17,35 @@ function UTILMakeUpdatable(object: UpdateQueryAnyGreeting) {
   if (object.enabled != undefined)
     Object.assign(data, { [makeProperty("enabled")]: object.enabled });
 
+  if (object.options != undefined) {
+    for (const key of Object.keys(object.options)) {
+      Object.assign(data, {
+        //@ts-expect-error Index Type Error
+        [makeProperty(`options.${key}`)]: object.options[key],
+      });
+    }
+  }
+
   return data;
 }
 
+@modelOptions({ options: { allowMixed: Severity.ALLOW } })
 export class SettingClass {
   @prop({ required: true })
   public guildId!: string;
 
-  @prop({
-    type: () => new Passthrough({ enabled: Boolean, channelId: String }, true),
-    default: { enabled: false, channelId: null },
-  })
+  @prop({ default: { enabled: false, channelId: null } })
   public welcome?: {
     enabled: boolean;
     channelId: string;
+    options: MessageOptionClass;
   };
 
-  @prop({
-    type: () => new Passthrough({ enabled: Boolean, channelId: String }, true),
-    default: { enabled: false, channelId: null },
-  })
+  @prop({ default: { enabled: false, channelId: null } })
   public farewell?: {
     enabled: boolean;
     channelId: string;
+    options: MessageOptionClass;
   };
 
   static async UPDATEFarewell(
@@ -75,10 +83,38 @@ export class SettingClass {
   }
 }
 
-interface UpdateQueryAnyGreeting {
+export class MessageOptionClass {
+  @prop({ type: () => String, default: null })
+  public content?: string;
+
+  @prop({
+    type: () => new Passthrough({ name: String, iconURL: String }, true),
+    default: { name: null, iconURL: null },
+  })
+  public author?: {
+    name: string;
+    iconURL: string;
+  };
+
+  @prop({ type: () => String })
+  public title?: string;
+
+  @prop({ type: () => String })
+  public description?: string;
+}
+
+export interface UpdateQueryAnyGreeting {
   enabled?: boolean;
-  channelId?: string;
+  channelId?: string | null;
+  options?: UpdateQueryMessageOption;
   prefix?: string;
+}
+
+export interface UpdateQueryMessageOption {
+  content: string;
+  title?: string | null;
+  description?: string | null;
+  author?: { name?: string | null; iconURL?: string | null };
 }
 
 export const Setting = getModelForClass(SettingClass, {
