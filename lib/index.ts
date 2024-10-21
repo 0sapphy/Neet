80217a2d54a42b5874a1f9a0b558e4f80a79ed00
Shook <160603390+Rayandevs@users.noreply.github.com>
2024-10-21 22:29:59 +0000

    removed: NeetButton

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
+/** CLIENT HELPERS */
 export * from "./Client/Neet";
 export * from "./Client/Event";
 export * from "./Client/CommandBuilder";
-export * from "./Client/NeetButton/NeetButton"
-export * from "./Client/NeetButton/NeetButtonParameters"
+
+/** UTILITY HELPERS */
+export * from "./Client/utils/NeetComponents";
