e1a7715e0b126771fed9dff8875812a0898e601d
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2025-05-07 19:15:59 +0000

    chore() cleanup

--- src/commands/slash/ping.ts
+++ src/commands/slash/ping.ts
@@ -4,6 +4,6 @@ import { SlashCommandStructure } from "../../../lib/DiscordJS/types.client";
 export default {
 	data: new SlashCommandBuilder().setName("ping").setDescription("Ping, Pong!"),
 	run(interaction, context) {
-		interaction.reply(`${codeBlock(">>>")} Ping: ${context.client.ws.ping / 1000}ms`)
+		interaction.reply(`${codeBlock(">>>")} Ping: ${context.client.ws.ping / 1000}ms`);
 	}
 } satisfies SlashCommandStructure;
--- src/events/Interactions/interactionCreate.ts
+++ src/events/Interactions/interactionCreate.ts
@@ -2,13 +2,13 @@ import { Neet } from "../../../lib";
 import { EventStructure } from "../../../lib/DiscordJS/types.client";
 
 export default {
-    name: "interactionCreate",
-    type: "on",
-    async run(interaction) {
-        if (!interaction.isChatInputCommand()) return;
-        const client = interaction.client as Neet<true>;
-        const command = client.commands.slash.get(interaction.commandName);
+	name: "interactionCreate",
+	type: "on",
+	async run(interaction) {
+		if (!interaction.isChatInputCommand()) return;
+		const client = interaction.client as Neet<true>;
+		const command = client.commands.slash.get(interaction.commandName);
 
-        command?.run(interaction, { client });
-    },
+		command?.run(interaction, { client });
+	}
 } satisfies EventStructure<"interactionCreate">;
